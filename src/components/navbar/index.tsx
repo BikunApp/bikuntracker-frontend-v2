5b39a40bfa1d6bbb501558827af603f2d035ac3b
Andrew Jeremy <andrew.jeremy@policymap.com>
2024-09-05 11:07:36 +0700

    initial commit

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="flex justify-between">
+      <div>Logo Here</div>
+      <div className="flex gap-4">
+        <div>Map</div>
+        <div>Info</div>
+      </div>
+    </div>
+  )
+}
