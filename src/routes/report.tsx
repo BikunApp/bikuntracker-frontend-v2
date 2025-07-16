c1cfdc81fed08d3126337f759cadae041abe590b
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-07-16 10:04:29 +0700

    feat: laporan

--- src/routes/report.tsx
+++ src/routes/report.tsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "@tanstack/react-router";
 import { createFileRoute } from "@tanstack/react-router";
-import { CircleAlert, Flag, PhoneCall } from "lucide-react";
+import { CircleAlert, Megaphone, Phone, Mail } from "lucide-react";
 
 import announcementPngUrl from "@/assets/icons/accouncement.png";
 import { Button } from "@/common/components/ui/button.tsx";
@@ -19,80 +19,82 @@ export default function Page() {
   const route = useLocation();
 
   return (
-    <div className="font-poppins flex min-h-dvh w-full flex-col items-center bg-[#F9F9FE] pb-20">
+    <div className="font-poppins flex min-h-dvh w-full flex-col items-center gap-6 bg-[#F9F9FE] pb-20">
       <div className="w-full bg-white py-4 text-center text-xl font-bold">
         Laporkan
       </div>
-      <div className="bg-primary-purple-100 h-[2px] w-full"></div>
-      <div className="mt-6">
-        <img
-          src={announcementPngUrl}
-          alt="Announcement Icon"
-          className="h-16 w-16"
-        />
-      </div>
-      <div className="flex w-full flex-col items-center px-8">
-        <div className="text-primary mt-5 text-center text-xl font-bold">
-          Ada yang bisa kami bantu?
-        </div>
-        <div className="text-400 mt-2 text-center text-xs">
-          Kami berkomitmen untuk menciptakan lingkungan yang aman dan nyaman
-          bagi seluruh pengguna. Jika kamu menemukan error pada aplikasi kami,
-          atau mengalami/menyaksikan pelecehan seksual, mohon laporkan kepada
-          kami.
-        </div>
-        <Button
-          onClick={() => {
-            if (user) {
-              navigate({ to: ROUTES.createReport });
-            } else {
-              sessionStorage.setItem(BackPathnameKey, route.pathname);
-              navigate({ to: ROUTES.ssoLogin });
-            }
-          }}
-          variant="danger"
-          className="mt-4 w-full rounded-2xl"
-          size="lg"
-        >
-          <Flag size={20} />
-          <div className="ml-3">Laporkan kekerasan seksual</div>
-        </Button>
-        <a
-          target="__blank"
-          rel="noopener noreferrer"
-          className="w-full"
-          href={ERROR_REPORT_URL}
-        >
+      <div className="font-poppins flex flex-col gap-5 px-20 max-lg:px-16 max-sm:px-6 md:px-10">
+        <div className="flex w-full flex-col gap-4">
+          <img
+            src="/assets/image1.png"
+            alt="Announcement"
+            className="w-1/2 max-w-[500px] self-center max-md:w-[70%]"
+          />
+          <div className="flex flex-col gap-1">
+            <h4 className="text-primary-purple-700 text-center font-bold">
+              Kami masih dalam pengembangan!
+            </h4>
+            <p className="text-center text-xs max-sm:text-justify">
+              Jika kamu menemukan bug atau kendala, bantu laporkan agar kami
+              bisa terus memperbaikinya! Partisipasi kamu sangat berarti untuk
+              menghadirkan layanan yang lebih baik bagi IKM UI.
+            </p>
+          </div>
           <Button
             variant="danger"
-            className="bg-danger-100 text-danger-500 hover:bg-danger-100/70 mt-4 w-full rounded-2xl"
-            size="lg"
+            className="rounded-2xl py-6 text-base font-bold"
           >
-            <CircleAlert size={20} />
-            <div className="ml-3">Laporkan error</div>
+            <CircleAlert className="mr-2 size-5 stroke-3" />
+            Laporkan Error
           </Button>
-        </a>
-        <div className="mt-5 flex w-full flex-col rounded-2xl bg-white p-5 shadow-md">
-          <div className="text-primary flex items-center">
-            <PhoneCall size={20} />
-            <div className="ml-4 font-bold">Call Center</div>
+        </div>
+        <div className="flex flex-col gap-4 rounded-[20px] bg-white px-6 py-4 shadow-md">
+          <div className="flex items-center gap-3 self-center">
+            <Megaphone className="text-primary-purple-700 size-6 stroke-2" />
+            <h4 className="text-primary-purple-700 text-center font-bold">
+              Laporkan Kekerasan Seksual
+            </h4>
           </div>
-          <div className="text-400 mt-3 text-xs font-medium">
-            Punya pertanyaan atau saran? Hubungi kontak kami di bawah ini:
+          <div className="flex w-1/2 flex-col items-center gap-3 self-center max-sm:w-full max-sm:flex-row">
+            <img
+              src="/assets/pelukan.svg"
+              alt="Violence"
+              className="w-[50px]"
+            />
+            <p className="text-center text-xs max-sm:text-justify">
+              Apabila mengalami atau mengetahui adanya tindak kekerasan seksual,
+              jangan ragu untuk menghubungi layanan hotline HopeHelps UI yang
+              tertera
+            </p>
           </div>
-          <a
-            target="__blank"
-            rel="noopener noreferrer"
-            className="w-full"
-            href={HELP_EMAIL_URL}
-          >
-            <Button
-              className="bg-primary-purple-100 text-primary hover:bg-primary-purple-100/70 hover:text-primary mt-4 w-full rounded-2xl"
-              size="lg"
-            >
-              help@ristek.cs.ui.ac.id
-            </Button>
-          </a>
+          <div className="flex w-full justify-center gap-2 font-bold">
+            <div className="flex w-full flex-col gap-1 text-center">
+              <p className="text-primary-purple-700 text-sm">Emergency*</p>
+              <div className="border-primary-purple-700 text-primary-purple-700 w-full rounded-full border-3 py-1 text-center">
+                24/7
+              </div>
+            </div>
+            <div className="flex w-full flex-col gap-1 text-center font-bold">
+              <p className="text-primary-purple-700 text-sm">
+                Operasional Umum
+              </p>
+              <div className="border-primary-purple-700 text-primary-purple-700 w-full rounded-full border-3 py-1 text-center">
+                24/7
+              </div>
+            </div>
+          </div>
+          <Button className="rounded-full py-6 text-base font-bold">
+            <Phone className="mr-2 size-5" />
+            0822-9978-8860
+          </Button>
+          <Button className="rounded-full py-6 text-sm font-bold">
+            <Mail className="mr-2 size-5" />
+            advokasi.hopehelps@gmail.com
+          </Button>
+          <p className="text-primary-purple-700 text-center text-xs">
+            *Jika dalam keadaan darurat, kirim pesan 'EMERGENCY' melalui
+            WhatsApp
+          </p>
         </div>
       </div>
     </div>
