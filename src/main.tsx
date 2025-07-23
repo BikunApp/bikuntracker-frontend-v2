56cbe7feb88a6dcae80d7846a978d33eceede5d7
Irfan Kamil <96167730+IrPanK@users.noreply.github.com>
2025-07-23 14:38:38 +0700

    Force Unregister Old Service Worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+}
+
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
