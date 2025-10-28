3834579cec8ab898328e612189125d6a0911bdf5
venedictchen <venedictchen@gmail.com>
2025-10-28 19:55:15 +0700

    feat: add modal for damri down payment issue or others

---
+++ src/common/constants/modalCopyWriting.ts
@@ -0,0 +1,11 @@
+export const DAMRI_DOWN = `
+Kami informasikan bahwa saat ini layanan Bikun Tracker tidak dapat diakses karena adanya 
+gangguan teknis. Kami memohon maaf atas ketidaknyamanan yang terjadi dan sedang melakukan 
+perbaikan agar layanan dapat segera beroperasi kembali.
+`;
+
+export const DEVELOPMENT = `
+Saat ini Bikun Tracker sedang dalam tahap pengembangan, sehingga
+beberapa data mungkin belum sepenuhnya akurat. Kami sedang bekerja
+keras untuk memperbaikinya agar pengalaman kamu semakin nyaman.
+`;
