f09a56f9786edad25fe0d71838b8997f981826e4
venedictchen <venedictchen@gmail.com>
2025-11-10 09:29:06 +0700

    feat: bikun change route modal copywriting

--- src/common/constants/modalCopyWriting.ts
+++ src/common/constants/modalCopyWriting.ts
@@ -4,3 +4,5 @@ Kami informasikan bahwa saat ini layanan Bikun Tracker tidak dapat diakses karen
 
 export const DEVELOPMENT = `Saat ini Bikun Tracker sedang dalam tahap pengembangan, sehingga beberapa data mungkin belum sepenuhnya akurat. Kami sedang bekerja keras untuk memperbaikinya agar pengalaman kamu semakin nyaman.
 `;
+
+export const CHANGE_ROUTE = `Kami informasikan bahwa jalur operasional Bikun telah mengalami perubahan dan akan berlaku secara permanen per tanggal 10 November 2025.\n\nSilakan melihat jalur terbaru untuk memastikan perjalanan Anda tetap sesuai tujuan.`
