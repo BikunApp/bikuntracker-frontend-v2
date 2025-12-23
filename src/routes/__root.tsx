5a0d9d8fe2c755f05da12a37a3ed95928759281e
muhfauzannn <fauzannmuhh@gmail.com>
2025-12-23 11:07:14 +0700

    feat: add SnowOverlay component for Christmas snow effect

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { useEffect } from "react";
 
 import Navbar from "@/common/components/navbar/index.tsx";
+import SnowOverlay from "@/common/components/snow/index.tsx";
 import useWebsocket from "@/common/hooks/useWebsocket.ts";
 import { useAuthStore } from "@/lib/store/auth.ts";
 import { useGlobalStore } from "@/lib/store/global.ts";
@@ -25,6 +26,7 @@ export default function RootComponent() {
 
   return (
     <div className="font-poppins relative h-dvh w-full">
+      <SnowOverlay />
       <Navbar />
       <Outlet />
     </div>
